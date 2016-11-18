bb9a62bb750c57a2a48b3b81b73b17e2158f25f7
mithun-Das <mithun.das227@gmail.com>
2016-11-19 03:58:30 +0600

    bug fixed

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
+use App\Models\User;
 use Validator;
 use Auth;
 use Hash;
@@ -143,7 +143,7 @@ class AuthController extends Controller
     }
 
     public function dashboard(){
-        return view('dashboard')
+        return view('admin.dashboard')
                     ->with('title','Dashboard')->with('user', Auth::user());
         // return 'Dashboard';
     }
