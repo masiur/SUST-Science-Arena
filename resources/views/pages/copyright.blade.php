d4e2a7d9dd7798bf45e47ca2f830bc6227fbc162
mithun-Das <mithun.das227@gmail.com>
2016-12-21 22:05:04 +0600

    structure construction

---
+++ resources/views/pages/copyright.blade.php
@@ -0,0 +1,9 @@
+
+
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="footer-btm">
+                            <span>&copy;2014. Theme by Design_mylife</span>
+                        </div>
+                    </div>
+                </div>
---
+++ resources/views/pages/script.blade.php
@@ -0,0 +1,48 @@
+
+
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery-migrate.min.js"></script> 
+        <!--bootstrap js plugin-->
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>       
+        <!--easing plugin for smooth scroll-->
+        <script src="js/jquery.easing.1.3.min.js" type="text/javascript"></script>
+        <!--sticky header-->
+        <script type="text/javascript" src="js/jquery.sticky.js"></script>
+        <!--flex slider plugin-->
+        <script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
+        <!--parallax background plugin-->
+        <script src="js/jquery.stellar.min.js" type="text/javascript"></script>
+        <!--digit countdown plugin-->
+        <script src="js/waypoints.min.js"></script>
+        <!--digit countdown plugin-->
+        <script src="js/jquery.counterup.min.js" type="text/javascript"></script>
+        <!--on scroll animation-->
+        <script src="js/wow.min.js" type="text/javascript"></script> 
+        <!--owl carousel slider-->
+        <script src="js/owl.carousel.min.js" type="text/javascript"></script>
+        <!--popup js-->
+        <script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        <!--you tube player-->
+        <script src="js/jquery.mb.YTPlayer.min.js" type="text/javascript"></script>        
+        <!--customizable plugin edit according to your needs-->
+        <script src="js/custom.js" type="text/javascript"></script>
+        <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+        <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+        <script type="text/javascript" src="js/revolution-custom.js"></script>
+        <!--cube portfolio plugin-->
+        <script src="cubeportfolio/js/jquery.cubeportfolio.min.js" type="text/javascript"></script>
+        <script src="js/cube-portfolio.js" type="text/javascript"></script>
+        <script src="js/pace.min.js" type="text/javascript"></script>
+                   <script src="video-background/core.js"></script>
+        <script src="video-background/transition.js"></script>
+        <script src="video-background/background.js"></script>
+        <script>
+              // Formstone Background - Video Background Settings
+    $(".video-section").background({
+        source: {
+            poster: "img/typing_cover.jpg",
+            mp4: "img/typing.mp4"
+        }
+    });
+        </script>
