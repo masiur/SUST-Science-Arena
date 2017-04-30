a557c3a6517b188ef709c16fba941316e0c5f1ae
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2017-05-01 05:27:58 +0600

    Code refactored

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,33 +1,25 @@
 <?php
 
-
 //Route::get('test',['as' => 'test', 'uses' => 'FrontendController@test']);
-
-
 Route::get('/', array('as' => 'index', 'uses' => 'FrontendController@index'));
-
-
 Route::get('news_list',['as' => 'news.list', 'uses' => 'NewsController@show']);
 Route::get('events',['as' => 'event.list', 'uses' => 'FrontendController@event']);
 Route::get('event/{id}',['as' => 'event.single', 'uses' => 'FrontendController@eventSingle']);
 Route::get('contact',['as' => 'contact', 'uses' => 'contactController@contact_page']);
 
+
 Route::get('executives',['as' => 'committee.member', 'uses' => 'MemberController@committee_member_list']);
 Route::post('contact_store',['as' => 'contact.store', 'uses' => 'contactController@store']);
-
-
-
 // public routes -- Added by Masiur
 Route::get('home', array('as' => 'home', 'uses' => 'FrontendController@index'));
 // Blog Public Page
+
 Route::get('blog',['as' => 'blog.index', 'uses' => 'FrontendController@blogPublicPage']);
 
 
 Route::group(['middleware' => 'guest'], function(){
-
 	Route::get('login', ['as'=>'login','uses' => 'Auth\AuthController@login']);
 	Route::post('login', ['as'=> 'postlogin','uses' => 'Auth\AuthController@doLogin']);
-
 	Route::get('register', ['as'=>'register','uses' => 'UserController@create']);
 	Route::post('register', ['as'=>'postRegister','uses' => 'UserController@store']);
 	
@@ -37,18 +29,12 @@ Route::group(['middleware' => 'guest'], function(){
 	// Password reset routes...
 	Route::get('password/reset/{token}', ['as' => 'getReset', 'uses' =>'Auth\PasswordController@getReset']);
 	Route::post('password/reset', ['as' => 'postReset', 'uses' => 'Auth\PasswordController@postReset']);
-
 	// social login route
 	Route::get('login/fb', ['as'=>'login/fb','uses' => 'SocialController@loginWithFacebook']);
 	Route::get('login/gp', ['as'=>'login/gp','uses' => 'SocialController@loginWithGoogle']);
-
 	
-
-
    
-
 });
-
 Route::group(array('middleware' => 'auth'), function()
 {
 	Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\AuthController@logout']);
@@ -56,11 +42,10 @@ Route::group(array('middleware' => 'auth'), function()
 	Route::get('settings', ['as' => 'settings', 'uses' => 'UserController@settings']);
 	Route::put('edit-profile', ['as' => 'edit.profile', 'uses' => 'UserController@postEditProfile']);
 
-	 Route::get('dashboard', array('as' => 'dashboard', 'uses' => 'Auth\AuthController@dashboard'));
 
+	Route::get('dashboard', array('as' => 'dashboard', 'uses' => 'Auth\AuthController@dashboard'));
 	Route::get('change-password', array('as' => 'password.change', 'uses' => 'Auth\AuthController@changePassword'));
 	Route::post('change-password', array('as' => 'password.doChange', 'uses' => 'Auth\AuthController@doChangePassword'));
-
 	// Blog CRUD for bloggers
 	Route::get('blog/myArticle',['as' => 'blog.myblog', 'uses' => 'BlogController@myblog']);
 	Route::get('blog/create',['as' => 'blog.create', 'uses' => 'BlogController@create']);
@@ -69,14 +54,11 @@ Route::group(array('middleware' => 'auth'), function()
 	Route::get('blog/{id}/show',['as' => 'blog.show', 'uses' => 'BlogController@show']);
 	Route::put('blog/{id}',['as' => 'blog.update', 'uses' => 'BlogController@update']);
 	Route::delete('blog/{id}',['as' => 'blog.delete', 'uses' => 'BlogController@destroy']);
-
     
 });
-
 Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function()
 {
 	// Route::get('dashboard', array('as' => 'dashboard', 'uses' => 'Auth\AuthController@dashboard'));
-
 	// Event CRUD by Mithun start
    Route::get('event',['as' => 'event.index', 'uses' => 'EventController@index']);
    Route::get('event/create',['as' => 'event.create', 'uses' => 'EventController@create']);
@@ -85,44 +67,32 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
    Route::get('event/{id}/edit',['as' => 'event.edit', 'uses' => 'EventController@edit']);
    Route::put('event/{id}',['as' => 'event.update', 'uses' => 'EventController@update']);
    Route::delete('event/{id}',['as' => 'event.delete', 'uses' => 'EventController@destroy']);
-
    // Mithun end
-
    // any new admin panel route must be added here 
     Route::get('user',['as' => 'user.index', 'uses' => 'UserController@index']);
 	Route::get('user/{id}/activation',['as' => 'user.activation', 'uses' => 'UserController@changeUserActivationStatus']);
 	Route::get('user/{id}/show',['as' => 'user.show', 'uses' => 'UserController@show']);
 	Route::put('user/{id}',['as' => 'user.update', 'uses' => 'UserController@update']);
 	Route::delete('user/{id}',['as' => 'user.delete', 'uses' => 'UserController@destroy']);
-
-
     // Pending Blog
     Route::get('read',['as' => 'blog.read', 'uses' => 'BlogController@read']);
 	Route::get('pending_blog',['as' => 'pending.blog', 'uses' => 'BlogController@pending_list']);
     Route::get('AdminSingleBlog/{id}',['as' => 'admin.blog.single', 'uses' => 'BlogController@AdminSingleBlog']);
     Route::get('acceptblog/{id}/',['as' => 'accept.blog', 'uses' => 'BlogController@AcceptBlog']);
     Route::get('ignoreblog/{id}/',['as' => 'ignore.blog', 'uses' => 'BlogController@ignoreBlog']);
-
 	
    // Member Add or Delete Or List Show
-
    Route::get('member/add',['as' => 'member.add',   'uses' => 'MemberController@add']);
    Route::get('member',['as' => 'member.list', 'uses' => 'MemberController@list']);
    Route::post('member/store',['as' => 'member.store', 'uses' => 'MemberController@store']);
    Route::get('member/{id}/edit',['as' => 'member.edit', 'uses' => 'memberController@edit']);
    Route::put('member/{id}',['as' => 'member.update', 'uses' => 'memberController@update']);
    Route::delete('member/{id}',['as' => 'member.delete', 'uses' => 'memberController@destroy']);
-
   // INFO Update
   
   Route::get('edit',['as' => 'info.edit',   'uses' => 'FrontendController@info_edit']);
   Route::put('info/{id}',['as' => 'info.update', 'uses' => 'FrontendController@info_update']);
-
-
-
 });
-
-
  //  NEWS CRUD
    Route::get('news',['as' => 'news.index', 'uses' => 'NewsController@index']);
    Route::get('news/create',['as' => 'news.create', 'uses' => 'NewsController@create']);
@@ -131,8 +101,6 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
    Route::get('news/{id}/edit',['as' => 'news.edit', 'uses' => 'NewsController@edit']);
    Route::put('project/{id}',['as' => 'project.update', 'uses' => 'ProjectController@update']);
    Route::delete('news/{id}',['as' => 'news.delete', 'uses' => 'NewsController@destroy']);
-
-
 	// Category CRUD
     Route::get('category',['as' => 'category.index', 'uses' => 'CategoryController@index']);
 	Route::get('category/create',['as' => 'category.create', 'uses' => 'CategoryController@create']);
@@ -142,15 +110,10 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
 	Route::put('category/{id}',['as' => 'category.update', 'uses' => 'CategoryController@update']);
 	Route::delete('category/{id}',['as' => 'category.delete', 'uses' => 'CategoryController@destroy']);
    
-
-
      //Content CRUD
-
      Route::get('content/create',['as' => 'content.create', 'uses' => 'ContentController@create']);
      Route::post('content',['as' => 'content.store', 'uses' => 'ContentController@store']);
   
-
-
 	/* will be Commented Out */
 	// Demo CRUD
 	Route::get('demo',['as' => 'demo.index', 'uses' => 'DemoController@index']);
@@ -160,10 +123,6 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
 	Route::get('demo/{id}/show',['as' => 'demo.show', 'uses' => 'DemoController@show']);
 	Route::put('demo/{id}',['as' => 'demo.update', 'uses' => 'DemoController@update']);
 	Route::get('demo/delete/{id}',['as' => 'demo.delete', 'uses' => 'DemoController@destroy']);
-
-
-
-
 	 // Language CRUD
 /*	Route::get('language',['as' => 'language.index', 'uses' => 'LanguageController@index']);
 	Route::get('language/create',['as' => 'language.create', 'uses' => 'LanguageController@create']);
@@ -172,6 +131,7 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], func
 	Route::get('language/{id}/show',['as' => 'language.show', 'uses' => 'LanguageController@show']);
 	Route::put('language/{id}',['as' => 'language.update', 'uses' => 'LanguageController@update']);
 	Route::delete('language/{id}',['as' => 'language.delete', 'uses' => 'LanguageController@destroy']);
-
 */
 Route::get('blog/{title}',['as' => 'blog.single', 'uses' => 'FrontendController@blogSingle']);
+
+
