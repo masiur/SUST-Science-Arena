e74ce6f6a0c38696d437e3eed3726be1ca1caa29
Masiur Rahman Siddiki <mrsiddiki@outlook.com>
2017-05-01 12:46:40 +0600

    shcema updated

--- database/migrations/2017_04_26_193604_create_SSA_Info_table.php
+++ database/migrations/2017_04_26_193604_create_SSA_Info_table.php
@@ -15,6 +15,7 @@ class CreateSSAInfoTable extends Migration
         Schema::create('Info', function (Blueprint $table) {
             $table->increments('id');
             $table->string('contact');
+            $table->string('fb_link');
             $table->string('twitter_link');
             $table->string('youtube_link');
             $table->string('googleplus_link');
